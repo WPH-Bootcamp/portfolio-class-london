a440d3cbd4e4589af6708ba1e74f9c3006dbfbdb
vincent-guizot <vincentguizot128@gmail.com>
2026-06-01 19:57:43 +0700

    initializations dan folders file structures

---
+++ profile-company/src/components/sections/AboutStats.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutStats = () => {
+  return <div>AboutStats</div>;
+};
+
+export default AboutStats;
---
+++ profile-company/src/components/sections/TrustedBrands.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrustedBrands = () => {
+  return <div>TrustedBrands</div>;
+};
+
+export default TrustedBrands;
